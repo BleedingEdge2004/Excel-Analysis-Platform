6c6a0ead3321c3cba60dfee4ec81c560db799d08
Omansh Sharma <omansh210305@gmail.com>
2025-07-07 17:06:58 +0530

    Initial Folder Setup

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json()); 
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+  res.send('Server is up and running!');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
