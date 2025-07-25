5c0d25f4d217949f57c19e34f0b2d60f52293e20
PERUGUMANOJ <manojperugu189@gmail.com>
2025-07-25 22:26:22 +0530

    Update Homepage.jsx

--- Frontend/src/Components/Homepage/Homepage.jsx
+++ Frontend/src/Components/Homepage/Homepage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import logo1 from "../../assets/logo1.svg";
-import img1 from '../../assets/img1.jpg';     
+import img1 from '../../assets/blue.png';
 import img2 from '../../assets/img2.png';
 import "./Homepage.css";
 
@@ -17,9 +17,10 @@ const Homepage = () => {
                     <ul className="nav-list">
                         <li><a href="#home">Home</a></li>
                         <li><a href="#about">About</a></li>
-                        <li><a href="/signin">Log In</a></li>
+
                     </ul>
-                </div>
+</div>
+<button ><a className="proflie" href="/signin">Log In</a></button>
             </div>
     
             <div className="image">
